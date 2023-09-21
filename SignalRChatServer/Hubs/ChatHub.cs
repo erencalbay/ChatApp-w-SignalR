956006432939beeadf263d38512dd8e32a5457f4
erencalbay <erencalbayfb@hotmail.com>
2023-09-21 12:01:19 +0300

    feat: Sending message to other clients.

--- SignalRChatServer/Hubs/ChatHub.cs
+++ SignalRChatServer/Hubs/ChatHub.cs
@@ -15,7 +15,24 @@ namespace SignalRChatServer.Hubs
             };
             ClientSource.Clients.Add(client);
             await Clients.Others.SendAsync("clientJoined", nickName);
-            Console.WriteLine(nickName);
+            await Clients.All.SendAsync("clients", ClientSource.Clients);
+        }
+
+        public async Task SendMessageASync(string message, string clientName)
+        {
+            Console.WriteLine(message);
+            clientName = clientName.Trim();
+            Client senderClient = ClientSource.Clients.FirstOrDefault(c => c.ConnectionId == Context.ConnectionId);
+            if (clientName == "Tümü")
+            {
+                await Clients.Others.SendAsync("receiveMessage", message);
+            }
+            else
+            {
+                Client client = ClientSource.Clients.FirstOrDefault(c => c.NickName == clientName);
+                await Clients.Client(client.ConnectionId).SendAsync("receiveMessage", message);
+                Console.WriteLine(message);
+            }
         }
     }
 }
